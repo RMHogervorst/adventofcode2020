533c25424855201360d1d43d8fa40f18c7ed2d12
Roel M. Hogervorst <hogervorst.rm@gmail.com>
2020-12-03 11:23:43 +0100

    and part 2

--- R/day3.R
+++ R/day3.R
@@ -79,4 +79,61 @@ create_sequenceround <- function(startpos){
     }
     values
 }
+N_trees <- function(ride_sequence){
+    sum(ride_sequence=="#")
+}
+N_trees(create_sequenceround(startpos))
 sum(create_sequenceround(startpos)=="#")
+### Part 2 ----------
+
+# Determine the number of trees you would encounter if, for each of the
+# following slopes, you start at the top-left corner and traverse the map all
+# the way to the bottom:
+    
+# Right 1, down 1.
+# Right 3, down 1. (This is the slope you already checked.)
+# Right 5, down 1.
+# Right 7, down 1.
+# Right 1, down 2.
+
+# In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s)
+# respectively; multiplied together, these produce the answer 336.
+
+# What do you get if you multiply together the number of trees encountered on
+# each of the listed slopes?
+# 
+
+
+### 
+# So now it is wise to generalize the next_step function.
+
+step_down_slope <- function(position, rowstep, colstep){
+    row <- position[1]+rowstep
+    col <- position[2]+colstep
+    col <- ifelse(col > 31, col-31, col)
+    c(row, col)
+}
+## and to make the steps dynamic.
+
+toboggan_ride <- function(startpos, down, right){
+    steps <- ceiling(max_row/down)
+    values <- character(length = steps)
+    
+    pos <- startpos
+    for (step in seq_len(steps)) {
+        values[step] <- retrieve_value(pos)
+        pos <- step_down_slope(pos, down, right)
+    }
+    values
+}
+r1d1 <- toboggan_ride(startpos, 1,1)
+r3d1 <- toboggan_ride(startpos, 1,3)
+## this should be the same as previous
+#all(r3d1 == create_sequenceround(startpos))
+r5d1 <-toboggan_ride(startpos, 1,5)
+r7d1 <-toboggan_ride(startpos, 1,7)
+r1d2 <-toboggan_ride(startpos, 2,1)
+
+list(r1d1,r3d1,r5d1,r7d1,r1d2) %>% 
+    purrr::map_dbl(N_trees) %>% # dbls in stead of integers because int gives an overflow!
+    purrr::reduce( `*`)
